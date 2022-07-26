8b0d0aac2bba7869b38f70f66377d24c0e65915a
Hemant <hrsmart2150@gmail.com>
2022-07-26 17:16:37 +0530

    Changed heading

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
                         <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
                     </svg>
-                    <span className="ml-3 text-xl">Image Uploader</span>
+                    <span className="ml-3 text-xl">Image Uploader with S3</span>
                 </a>
                 <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
                     <a className="mr-5 text-gray-900 font-bold">Hello there 👋🏼</a>
